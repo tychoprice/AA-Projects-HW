197a750d03ea8d403652e8ed9a9235c2a5e01855
Tycho Price <tychoprice@gmail.com>
2017-07-04 00:01:47 -0700

    mymap complete

--- WXDX/W5D3/js_intro/phase_2_enumerable.js
+++ WXDX/W5D3/js_intro/phase_2_enumerable.js
@@ -1,7 +1,47 @@
+const NUMS = [1, 2, 3, 4, 5];
+
+//myEach
 Array.prototype.myEach = function(callback) {
   for (let i = 0; i < this.length; i++) {
     callback(this[i]);
   }
 };
 
-this.myEach
+//anonymous function passed as argument
+NUMS.myEach(function(num) {
+  console.log(num * num);
+});
+
+//example callback function
+function printOdds(num) {
+  if (num % 2 === 0) {
+    console.log(num);
+  }
+}
+
+//function passed as argument
+NUMS.myEach(printOdds); // => 2, 4
+
+
+Array.prototype.myEach = (callback) => {
+  for (let i = 0; i < this.length; i++) {
+    callback(this[i]);
+  }
+};
+
+NUMS.myEach(function(num) {
+  console.log(num * num);
+});
+
+//myMap
+Array.prototype.myMap = function(callback) {
+  const mapped = [];
+  for (let i = 0; i < this.length; i++) {
+    mapped.push(callback(this[i]));
+  }
+  return mapped;
+};
+
+NUMS.myMap(function cubed(num) {
+  return num * num * num;
+});
